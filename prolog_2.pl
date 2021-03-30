07e6650f46ffd6bd07e820c93087a5c5d9efbf07
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2021-03-30 16:20:28 +0300

    Create prolog_2.pl

---
+++ prolog_2.pl
@@ -0,0 +1,22 @@
+;(setq i 0)
+;(loop
+ ;(setq i (+ i 1))
+ ;(print i)
+ ;(when(>= i 10) (return 0)) 
+ ;)
+
+;(loop for x in (list 1 2 3 4)
+;      do (print (* x x))
+;      )
+
+;(dotimes (x 8)
+;    (print (+ x 1))
+;    )
+
+;(do (
+;     (x 0 (+ 1 x)) 
+;     (y 20 (- y 2))
+;     )
+;    )
+; parametre olarak aldığı listedeki 3'e tam bölünen
+;sayıların listesini dönderen lambda fonksiyonu
