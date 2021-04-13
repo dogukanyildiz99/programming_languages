b3e7abbf486159a02cf644d591f234878fab9d71
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2021-04-13 15:18:43 +0300

    Create list_in_prolog.pl
    liste işlemleri

---
+++ list_in_prolog.pl
@@ -0,0 +1,31 @@
+| ?- [X,Y,Z,T] = [a,b,c,[d,e]].
+
+T = [d,e]
+X = a
+Y = b
+Z = c
+
+yes
+| ?- [H|T] = [a,b,c,d,e]
+
+H = a
+T = [b,c,d,e]
+
+yes
+| ?- [_,X,_,_] = [a,b,c,d].
+
+X = b
+
+yes
+| ?- [_,X,_,_|T] = [a,b,c,d].
+
+T = []
+X = b
+
+yes
+| ?- [_,_,[X|Y],_,Z|T] = [a,b,[c,d,e],f,g,h].
+
+T = [h]
+X = c
+Y = [d,e]
+Z = g
