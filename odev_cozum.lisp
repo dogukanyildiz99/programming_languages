1110234e3f7d3c0c9f06b8e5ab6e561f77c1eaf5
Doğukan YILDIZ <56637126+dogukanyildiz99@users.noreply.github.com>
2021-04-13 16:14:36 +0300

    Create odev_cozum.lisp

---
+++ odev_cozum.lisp
@@ -0,0 +1,8 @@
+(print ((lambda (L)
+        (defvar R nil)
+        (dolist (x L)
+            (if (eq (mod x 3) 0) (push x R))
+            )
+        R) (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+    )
+)
